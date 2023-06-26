9b458b9efc67c9d43166ba5b954576a3162a073d
Ruslan Pislyakov <Ruslan.Pislyakov@redsailtechnologies.com>
2023-06-25 22:02:45 -0700

    Assignment_1

---
+++ js/script.js
@@ -0,0 +1,120 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// let numericExm = 12;
+// let stringExm = 'test';
+// let booleanExm = true;
+// let nullExm = null;
+
+// STEP 3
+// let a = 12 + (3 * 4);
+// let b = 10 - 12 / 4;
+
+// STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let strZipCode;
+// let numYourAge;
+// let strReferralSource;
+// let bMayWeContactYou;
+
+// STEP 5
+// let strFirstName;
+// strFirstName = 'Ruslan';
+
+// let strLastName = 'Pisliakov';
+
+// var bMayWeContactYou = true;
+
+// STEP 6
+// let a = true;
+// let b = '20';
+// alert(a + b);
+
+// STEP 7
+// let a = true + 'Pleasure';
+// alert(a);
+
+// let b = 100 + true;
+// alert(b);
+
+// STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// STEP 9
+// let a = null;
+// window.console.log(a);
+
+// let b = 'Ruslan';
+// window.console.log(parseInt(b));
+
+// STEP 10
+// window.console.log(typeof 'Ruslan');
+// window.console.log(typeof 12);
+// window.console.log(typeof true);
+// window.console.log(typeof {x:1, y:2});
+// window.console.log(typeof a);
+
+// STEP 11
+// alert('Hello ' + 'Ruslan Pisliakov' + ', welcome to the JavaScript class!');
+
+// STEP 12
+// let fullName = 'Ruslan Pisliakov';
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!');
+
+// STEP 13
+// let fullName = 'Ruslan Pisliakov';
+// let course = 'JavaScript';
+// alert('Hello ' + fullName + ', welcome to the ' + course + ' class!');
+
+// STEP 14
+// let fullName = 'Ruslan Pisliakov';
+// let course = 'JavaScript';
+// alert('Hello ' + fullName + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 15
+// let fullName = prompt('Hi! What is your name?');
+// let course = 'JavaScript';
+// alert('Hello ' + fullName + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 16
+// let fullName = 'Ruslan Pisliakov';
+// let course = prompt('Hi! What is your class name?');
+// alert('Hello ' + fullName + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// window.console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// window.console.log(x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// window.console.log(x);
+
+// STEP 20
+// The task is uncleared for me
+
+// STEP 21
+// let x = 4;
+// let y = 6;
+// window.console.log(x == 4 && y == 6);
+
+// STEP 22
+// let x = 4;
+// let y = 6;
+// window.console.log(x == 5 || y == 22);
